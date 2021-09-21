0412402db64b165da6dbf0352edd0d8a89dcb1a8
Stanislav_Chyrva <9104948@gmail.com>
2021-09-21 13:27:29 +0300

    asd

--- chunk-24.83a98a1038d43c080269.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{830:function(n,o,w){"use strict";w.r(o);o.default=function(){return window.parent.postMessage(window.location.search),null}}}]);
